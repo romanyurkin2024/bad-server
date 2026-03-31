379a12d657aadcb8cf1c2ddc942fa6d3a3db0a98
romanyurkin2024 <tsoyroman2024@yandex.kz>
2026-03-31 14:10:32 +0500

    Проверка загрузки файлов

--- backend/src/middlewares/ratelimiter.ts
+++ backend/src/middlewares/ratelimiter.ts
@@ -2,9 +2,9 @@ import { RateLimiterMemory } from 'rate-limiter-flexible'
 import { Request, Response, NextFunction } from 'express'
 
 const rateLimiter = new RateLimiterMemory({
-    points: Number(process.env.RATE_LIMIT_POINTS) || 3,     
-    duration: Number(process.env.RATE_LIMIT_DURATION) || 5,  
-    blockDuration: Number(process.env.RATE_LIMIT_BLOCK) || 15,
+    points: Number(process.env.RATE_LIMIT_POINTS) || 20,     // кол-во запросов
+    duration: Number(process.env.RATE_LIMIT_DURATION) || 1,  // за сколько секунд
+    blockDuration: Number(process.env.RATE_LIMIT_BLOCK) || 15, // блокировка на сколько секунд
 })
 
 const limiter = async (req: Request, res: Response, next: NextFunction) => {
