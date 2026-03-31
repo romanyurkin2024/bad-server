c39670f5106a6f09889604024714c2af23c1c175
romanyurkin2024 <tsoyroman2024@yandex.kz>
2026-03-31 14:17:53 +0500

    Проверка загрузки файлов

--- backend/src/middlewares/ratelimiter.ts
+++ backend/src/middlewares/ratelimiter.ts
@@ -7,10 +7,17 @@ const rateLimiter = new RateLimiterMemory({
     blockDuration: Number(process.env.RATE_LIMIT_BLOCK) || 15, // блокировка на сколько секунд
 })
 
+const excludedPaths = ['/auth/csrf-token', '/auth/login', '/auth/register']
+
 const limiter = async (req: Request, res: Response, next: NextFunction) => {
     if (process.env.RATE_LIMITED !== 'true') {
         return next()
     }
+
+    if (excludedPaths.includes(req.path)) {
+        return next()
+    }
+
     try {
         await rateLimiter.consume(req.ip || 'unknown')
         return next()
